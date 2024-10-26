2ce235435b1747636afdf736d3a9cb8f7bdc9db2
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-10-26 15:03:53 -0300

    Config the path for all the html files

--- login-service/app.js
+++ login-service/app.js
@@ -1,11 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from 'path';
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
+// Fix para __dirname
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 //Server
 const app = express(); 
+
+// Configuraciones para los archivos estaticos, osea para las decoraciones
+//app.use(express.static(__dirname + "public"));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+
+
+
+
+
+
+
+
 const port = process.env.LOGIN_PORT; 
 app.listen(port, (error) => {
     if (error){
