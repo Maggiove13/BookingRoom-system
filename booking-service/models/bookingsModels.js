2b943c524f8fc451c8e86a7754162584577581ee
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-10-26 17:37:10 -0300

    Create a new pool config

--- booking-service/models/bookingsModels.js
+++ booking-service/models/bookingsModels.js
@@ -2,3 +2,12 @@ import { dbConfig } from "../config/config.js";
 import pg from "pg";
 
 const { Pool } = pg;
+
+
+const pool = new Pool({
+    host: dbConfig.host,
+    user: dbConfig.user,
+    password: dbConfig.password,
+    database: dbConfig.database,
+    port: dbConfig.port
+});
