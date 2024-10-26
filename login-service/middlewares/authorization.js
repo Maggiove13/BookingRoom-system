dbe792f420754be6fe6fab937ec6ae5fa41bf802
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-10-26 16:23:57 -0300

    fix the connection-db export route

--- login-service/middlewares/authorization.js
+++ login-service/middlewares/authorization.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 import dotenv from "dotenv";
-import pool from "../connection-db.js";  // Importamos la conexión a la base de datos
+import { pool } from '../models/connection-db.js';
 
 dotenv.config();
 
