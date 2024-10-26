7aed975b694eca87de43d6feecea9db3e9749e4c
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-10-26 16:58:55 -0300

    importing the libreries

---
+++ room-service/controllers/roomControllers.js
@@ -0,0 +1,4 @@
+import { queryAddRoom } from "../models/roomModel.js"; //Funcion de Models
+import { queryAvailableRoom } from "../models/roomModel.js"; //Funcion de Models
+import { queryUpdateStatusRoom } from "../models/roomModel.js";
+import amqp from "amqplib";
