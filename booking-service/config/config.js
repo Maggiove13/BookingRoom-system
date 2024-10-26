350d6337fa5347fd283dbe6aaea50afb7d432e77
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-10-26 17:33:07 -0300

    create export object db config

--- booking-service/config/config.js
+++ booking-service/config/config.js
@@ -1,3 +1,12 @@
 import dotenv from "dotenv";
 
 dotenv.config();
+
+
+export const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    port: process.env.DB_PORT
+}
