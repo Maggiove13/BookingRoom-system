d51bc195debd54c0f797dd568cd8cbbac40e8ac4
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-10-26 17:00:05 -0300

    create function addRoom

--- room-service/controllers/roomControllers.js
+++ room-service/controllers/roomControllers.js
@@ -2,3 +2,14 @@ import { queryAddRoom } from "../models/roomModel.js"; //Funcion de Models
 import { queryAvailableRoom } from "../models/roomModel.js"; //Funcion de Models
 import { queryUpdateStatusRoom } from "../models/roomModel.js";
 import amqp from "amqplib";
+
+
+export const addRoom = async (req, res) => {
+    const {type, capacity, status} = req.body;
+    try {
+        await queryAddRoom(type, capacity, status);
+        res.status(200).send({message: "Room added successfully"});
+    } catch (error) {
+        res.status(500).send({message: "Internal server error"});
+    }
+}
