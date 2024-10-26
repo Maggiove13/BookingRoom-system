133ea79e14fdf8709838a113012957530deb81ed
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-10-26 16:36:02 -0300

    config the server

--- room-service/app.js
+++ room-service/app.js
@@ -4,3 +4,11 @@ import { roomsRoutes } from "./routes/roomRoutes.js";
 import cookieParser from "cookie-parser"; //npm install 'cookie-parser'
 
 dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server runnig at port ${PORT}`);
+});
