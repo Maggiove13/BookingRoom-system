ff20de6460d15ed656f227372758e2810b00d3dc
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-10-26 14:46:30 -0300

    export the config for the connection to the database

---
+++ login-service/models/connection-db.js
@@ -0,0 +1,20 @@
+import { dbConfig } from "../config/config.js";
+import pg from "pg";
+
+const { Pool } = pg;
+
+const pool = new Pool({
+    host: dbConfig.host,
+    user: dbConfig.user,
+    password: dbConfig.password,
+    database: dbConfig.database,
+    port: dbConfig.port
+});
+
+
+if (!pool){
+    console.log("There was an error connecting to the database", pool.name);
+    console.error("There was an error:", error)
+} else {
+    console.log("Connected to de database");
+}
