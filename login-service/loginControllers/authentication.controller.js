31c07e20f12efae1cb40a8dd90c8d31cab47170b
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-10-26 16:11:33 -0300

    change the route of exportation

--- login-service/loginControllers/authentication.controller.js
+++ login-service/loginControllers/authentication.controller.js
@@ -1,6 +1,6 @@
 //Este es el controlador de autenticacion
 
-import pool from "../connection-db.js";  
+import pool from "../models/connection-db.js" 
 import bcryptjs from "bcryptjs"; 
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
