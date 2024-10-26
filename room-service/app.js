1782f20d98e56bc569ba5c94fa33751f5aa2b7e9
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-10-26 16:34:25 -0300

    importing the libreries

---
+++ room-service/app.js
@@ -0,0 +1,4 @@
+import express from "express";
+import dotenv from "dotenv";
+import { roomsRoutes } from "./routes/roomRoutes.js";
+import cookieParser from "cookie-parser"; //npm install 'cookie-parser'
