473612a26bbb1f07801e9a095c88af9fac574aec
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-10-26 17:57:57 -0300

    create makeBooking function

--- booking-service/controllers/bookingControllers.js
+++ booking-service/controllers/bookingControllers.js
@@ -5,3 +5,43 @@ import { queryUpdateBookingStatus } from "../models/bookingsModels.js";
 import amqp from "amqplib";
 import CircuitBreaker from 'opossum';
 import axios from "axios";
+
+
+export const makeBooking = async (req, res) => {
+
+    const {user_name, room_id, start_date, end_date} = req.body;
+
+    const status = "pending"; 
+
+    try {
+        const loginUrl = `http://localhost:4000/api/login/${user_name}`;
+        const userResponse = await axios.get(loginUrl);
+        const user_id = userResponse.data.user_id;
+        const result = await queryInsertBooking(user_id, room_id, start_date, end_date, status);
+
+        if (result.rowCount === 0){
+            return res.status(400).send({message: "Error: Booking not created"});
+        }
+
+        const booking = result.rows[0]; // Obtener la primera fila de la respuesta
+
+        console.log("Booking result:", booking);
+
+        // Enviar el room_id y el status del room a la RabbitMQ
+
+        const roomAvailability = 'not available'; // Una vez que haces la reserva, el room queda reservado.
+
+        // Usar el Circuit Breaker para enviar el mensaje
+        breaker.fire(roomAvailability, booking.room_id).catch((error) => {
+            console.error("Error al enviar el mensaje:", error);
+        })
+
+
+        res.status(200).send({message: "Booking Successful"});
+
+    } catch (error){
+        console.error("Error, creating a booking", error);
+        res.status(500).send({message: "Error creating the booking"});
+    }
+}
+
