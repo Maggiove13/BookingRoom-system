4b056967b2911e4ef1d7f7785d6a02ec0fc5329a
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-10-26 16:35:08 -0300

    load the environment variables

--- room-service/app.js
+++ room-service/app.js
@@ -2,3 +2,5 @@ import express from "express";
 import dotenv from "dotenv";
 import { roomsRoutes } from "./routes/roomRoutes.js";
 import cookieParser from "cookie-parser"; //npm install 'cookie-parser'
+
+dotenv.config();
