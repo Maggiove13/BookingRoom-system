a2348724f838e36e10018dca128a684efab4f5a9
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-10-26 16:43:09 -0300

    Create a pool of connections

--- room-service/models/roomModel.js
+++ room-service/models/roomModel.js
@@ -3,3 +3,12 @@ import pg from "pg";  // Usando pg para la conexión a PostgreSQL
 
 
 const { Pool } = pg;
+
+const pool = new Pool({
+    host: databaseConfig.host,
+    user: databaseConfig.user,
+    password: databaseConfig.password,
+    database: databaseConfig.name,
+    port: databaseConfig.port
+});
+
