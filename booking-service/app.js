5ee8d71328b5e97fe30b7000fe4299874034f2ce
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-10-26 17:26:22 -0300

    config the listen port for the server

--- booking-service/app.js
+++ booking-service/app.js
@@ -10,3 +10,14 @@ const app = express();
 app.use(express.json());
 
 app.use(cookieParser());
+
+
+
+const port = process.env.PORT;
+
+app.listen(port, (error) => {
+    if (error){
+        console.log("Error starting the server", error);
+    }
+    console.log(`Server running at port ${port}`);
+});
